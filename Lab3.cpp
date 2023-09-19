b4908c203aec7f071cab2a0cabeb6c733ec9a6df
mank000 <kozmin_arti@mail.ru>
2023-09-19 17:08:21 +0700

    почти готово

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <clocale>
+#include <cmath>
 using namespace std;
 
 int solution1() {
@@ -15,10 +16,21 @@ int solution2() {
 	cout << num1 << "\n" << num2 << "\n" << num3 << "\n";
 	return 0;
 }
+int solution3() {
+	const float a = 0.004;
+	const int b = 42;
+	double UserNum = 0.0;
+	cin >> UserNum;
+	if (UserNum <= 10) cout << cbrt((pow((UserNum + 1),2))) << "\n";
+	else if (10 < UserNum and UserNum <= 15) cout << ((1 / ((pow(UserNum, 3)) + 2)) + b) << "\n";
+	else if (UserNum > 15) cout << ((pow(UserNum, 2)) - (a * UserNum)) << "\n";
+	return 0;
+}
 
 int main() {
+	cout.precision(15);
 	setlocale(LC_ALL, "ru");
-	cout << solution2();
+	cout << solution3();
 	return 0;
 }
 
